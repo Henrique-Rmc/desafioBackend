e5ce6e228b4c5ceb3d62e6df270c6a8a076e31b7
Henrique-Rmc <paulo.cruz@ccc.ufcg.edu.br>
2024-08-22 16:41:00 -0300

    Patch: Refactoring mensagemService

--- services/mensagemService.js
+++ services/mensagemService.js
@@ -1,60 +1,42 @@
-const { v4: uuidv4 } = require("uuid");
 const { Mensagem, Cliente } = require("../models");
+const Generator = require("../utils/generator"); // Importando a nova classe Generator
 
 class MensagemService {
 
 
-
-	static generateCpfCnpj() {
-		return Math.floor(10000000000 + Math.random() * 90000000000).toString();
-	}
-
-	static generateValor() {
-		return parseFloat((Math.random() * 1000).toFixed(2));
-	}
-
-	static generateCliente(tipo, ispb) {
-		return {
-			nome: `${tipo} ${Math.floor(Math.random() * 1000)}`,
-			cpfCnpj: MensagemService.generateCpfCnpj(),
-			ispb: ispb || Math.floor(10000000 + Math.random() * 90000000).toString(),
-			agencia: Math.floor(1000 + Math.random() * 9000).toString(),
-			contaTransacional: Math.floor(100000 + Math.random() * 900000).toString(),
-			tipoConta: ["CACC", "SVGS"][Math.floor(Math.random() * 2)],
-		};
-	}
-
+	
 	static async generateRandomMensagens(ispb, count) {
-		const mensagens = [];
+		try {
+			const mensagens = [];
 
-		const recebedorData = MensagemService.generateCliente("Recebedor", ispb);
-		const [recebedor] = await Cliente.findOrCreate({
-			where: { cpfCnpj: recebedorData.cpfCnpj },
-			defaults: recebedorData,
-		});
-
-		for (let i = 0; i < count; i++) {
-			const pagadorData = MensagemService.generateCliente("Pagador");
-			const [pagador] = await Cliente.findOrCreate({
-				where: { cpfCnpj: pagadorData.cpfCnpj },
-				defaults: pagadorData,
+			const recebedorData = Generator.generateCliente("Recebedor", ispb);
+			const [recebedor] = await Cliente.findOrCreate({
+				where: { cpfCnpj: recebedorData.cpfCnpj },
+				defaults: recebedorData,
 			});
 
-			const mensagem = {
-				endToEndId: `E${ispb}${new Date().getTime()}${i}`,
-				valor: MensagemService.generateValor(),
-				campoLivre: "",
-				txId: uuidv4(),
-				dataHoraPagamento: new Date(),
-				pagadorId: pagador.id,
-				recebedorId: recebedor.id,
-			};
+			for (let i = 0; i < count; i++) {
+				const pagadorData = Generator.generateCliente("Pagador");
+				const [pagador] = await Cliente.findOrCreate({
+					where: { cpfCnpj: pagadorData.cpfCnpj },
+					defaults: pagadorData,
+				});
 
-			mensagens.push(mensagem);
+				const mensagem = Generator.generateMensagem(
+					ispb,
+					pagador.id,
+					recebedor.id,
+					i
+				);
+
+				mensagens.push(mensagem);
+			}
+
+			await Mensagem.bulkCreate(mensagens);
+			return mensagens;
+		} catch (error) {
+			throw new Error("Error generating messages");
 		}
-
-		await Mensagem.bulkCreate(mensagens);
-		return mensagens;
 	}
 }
 
---
+++ utils/generator.js
@@ -0,0 +1,36 @@
+const { v4: uuidv4 } = require("uuid");
+
+class Generator {
+	static generateCpfCnpj() {
+		return Math.floor(10000000000 + Math.random() * 90000000000).toString();
+	}
+
+	static generateValor() {
+		return parseFloat((Math.random() * 1000).toFixed(2));
+	}
+
+	static generateCliente(tipo, ispb) {
+		return {
+			nome: `${tipo} ${Math.floor(Math.random() * 1000)}`,
+			cpfCnpj: Generator.generateCpfCnpj(),
+			ispb: ispb || Math.floor(10000000 + Math.random() * 90000000).toString(),
+			agencia: Math.floor(1000 + Math.random() * 9000).toString(),
+			contaTransacional: Math.floor(100000 + Math.random() * 900000).toString(),
+			tipoConta: ["CACC", "SVGS"][Math.floor(Math.random() * 2)],
+		};
+	}
+
+	static generateMensagem(ispb, pagadorId, recebedorId, index) {
+		return {
+			endToEndId: `E${ispb}${new Date().getTime()}${index}`,
+			valor: Generator.generateValor(),
+			campoLivre: "",
+			txId: uuidv4(),
+			dataHoraPagamento: new Date(),
+			pagadorId,
+			recebedorId,
+		};
+	}
+}
+
+module.exports = Generator;
